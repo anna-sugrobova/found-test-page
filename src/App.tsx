6ca337d4c8f9db5841c117fe1c682f2b9fc07d5a
Anna Sugrobova <a.sugrobova@pik-remont.ru>
2022-09-24 23:38:52 +0700

    add fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import './index.css';
 
 function App() {
   return (
     <div className="App">
+      <header>
+        Custom domains
+        <p>Custom domains</p>
+      </header>
     </div>
   );
 }
 
 export default App;
+
