0cbf72a9825bae7dfa2ed498cde743f22b6f51c2
Anna Sugrobova <a.sugrobova@pik-remont.ru>
2022-09-25 20:45:41 +0700

    add PrimaryDomain component

---
+++ src/components/PrimaryDomain/PrimaryDomain.tsx
@@ -0,0 +1,41 @@
+import { InfoBlock } from '../InfoBlock/InfoBlock';
+import { ReactComponent as ArrowHook } from '../../assets/ArrowHook.svg';
+import { ReactComponent as Edit } from '../../assets/Edit.svg';
+import { ReactComponent as Share } from '../../assets/Share.svg';
+import { ReactComponent as Delete } from '../../assets/Delete.svg';
+
+import './PrimaryDomain.scss';
+
+function PrimaryDomain() {
+  return (
+    <div className="primary-domain">
+      <InfoBlock>
+        <div className="heading">
+          <p className="subtitle">Primary domain</p>
+          <h2>Amended Taxretum</h2>
+          <a href="amendedtaxreturn.com">amendedtaxreturn.com</a>
+        </div>
+        <div className="properties">
+          <span className="status-title">Status</span>
+          <p>Submitted</p>
+        </div>
+        <div className="icons">
+          <div className="icon">
+            <ArrowHook />
+          </div>
+          <div className="icon">
+            <Edit />
+          </div>
+          <div className="icon">
+            <Share />
+          </div>
+          <div className="icon">
+            <Delete />
+          </div>
+        </div>
+      </InfoBlock>
+    </div>
+  );
+}
+
+export { PrimaryDomain };
